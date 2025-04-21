79854ec95d15c78c22c14628d8549790d4f74e96
alirezafay <149387054+alirezafay@users.noreply.github.com>
2025-04-21 13:24:24 +0330

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -26,7 +26,7 @@ def load_json_from_google_drive(file_id):
             raise Exception("The file is not a valid JSON format.")
     else:
         raise Exception(f"Failed to download JSON: {response.status_code}")
-file_id = "1EgG5Cc3iTr1xwSYl6p0FR25juejGxTHR"
+file_id = "1z7MSTiwVUKeFVzAUZInPFb2AB8neDPF-"
 file_id2 ="1kqCbNDU_Lfld9YRXTdaSX-4h8LaObDdV"  
 user_data = load_json_from_google_drive(file_id)
 
